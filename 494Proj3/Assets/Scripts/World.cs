4955b279c8180ef0a10e970e1f56406c1d57b7f3
Morgan Erlich <nagrom525@gmail.com>
2016-10-23 23:48:11 -0400

    All code written this week

---
+++ 494Proj3/Assets/Scripts/World.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction { LEFT, RIGHT, UP, DOWN};
+
+public class World : MonoBehaviour {
+    enum WorldState { NORMAL, ROTATING, RUNNING_LEVEL, PAUSED}
+    enum WorldSideActive { NEG_X, POS_X, NEG_Y, POS_Y, NEG_Z, POS_Z}
+    public static World S;
+    public float roationTime;
+
+    private WorldState current_state;
+
+
+    void Awake() {
+        if(S != null) {
+            Debug.Log("Singleton can only be set once");
+        } else {
+            S = this;
+        }
+        current_state = WorldState.NORMAL;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+ 
+}
---
+++ 494Proj3/Assets/Scripts/WorldRotationButton.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldRotationButton : MonoBehaviour {
+    enum RotationButtonState { ROTATING_MOUSE, ROTATING_WORLD, NORMAL}
+    public float speed = 1.0f;
+    public float rotationCutoff = 25.0f;
+    public float fullRotationTime = 1.0f;
+
+    /////////////////////////////////////////
+
+
+    private float degreesY;
+    private RotationButtonState current_state = RotationButtonState.NORMAL;
+    private Quaternion rotationStart;
+    private Quaternion rotationEnd;
+    private Vector3 prevBaseRotation;
+
+    // handle the auto rotation after the user lets go of the wheel
+    private Vector3 endAutoRotationValue;
+    private Vector3 startAutoRotationValue;
+    private float autoRotationStartTime;
+    private float timeToAutoRotate;
+
+    void Awake() {
+        prevBaseRotation = this.transform.rotation.eulerAngles;
+    }
+
+    // Use this for initialization
+    void Start() {
+        degreesY = transform.rotation.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        switch (current_state) {
+            case RotationButtonState.NORMAL:
+                NormalMouseListening();
+                break;
+            case RotationButtonState.ROTATING_MOUSE:
+                NormalMouseListening();
+                break;
+            case RotationButtonState.ROTATING_WORLD:
+                RotatingWorld();
+                break;
+        }
+       
+    }
+
+    void NormalMouseListening() {
+        if (Input.GetMouseButton(0) && (current_state == RotationButtonState.NORMAL || current_state == RotationButtonState.ROTATING_MOUSE)) {
+            // case that the user is actively rotating the wheel
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                WorldRotationButton button = hit.collider.gameObject.GetComponent<WorldRotationButton>();
+                if (button != null) {
+                    // then the mouse key is clickd and it is also over the rotation button
+                    current_state = RotationButtonState.ROTATING_MOUSE;
+                    degreesY = this.transform.rotation.eulerAngles.y;
+                    degreesY -= Input.GetAxis("Mouse X") * speed;
+                    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, degreesY, 0);
+                    World.S.transform.rotation = this.transform.rotation;
+                }
+            }
+        } else if (current_state == RotationButtonState.ROTATING_MOUSE) {
+            // case that the user lets go of the wheel
+            current_state = RotationButtonState.ROTATING_WORLD;
+            // we have to round the value back to the nerest rotation (only 4 possiable worlds)
+            float rotationChange =  Mathf.DeltaAngle(this.transform.rotation.eulerAngles.y, prevBaseRotation.y);
+
+            if (Mathf.Abs(rotationChange) < rotationCutoff) {
+                // then send it back to the previous rotation
+                endAutoRotationValue = prevBaseRotation;
+
+            } else if(rotationChange >= rotationCutoff){
+                // then find the direction to rotate it
+                endAutoRotationValue.y = Mathf.Floor(transform.rotation.eulerAngles.y / 90) * 90;
+            } else {
+                endAutoRotationValue.y = Mathf.Ceil(transform.rotation.eulerAngles.y / 90) * 90;
+            }
+          
+            startAutoRotationValue = transform.rotation.eulerAngles;
+            autoRotationStartTime = Time.time;
+            prevBaseRotation = endAutoRotationValue;
+            timeToAutoRotate = fullRotationTime * (Mathf.Abs(rotationChange) / 90.0f);
+        }
+    }
+
+    void RotatingWorld() {
+        // figure out which way to finish the rotation
+        float t = (Time.time - autoRotationStartTime) / timeToAutoRotate;
+        if (t > 1) {
+            transform.rotation = Quaternion.Euler(endAutoRotationValue);
+            current_state = RotationButtonState.NORMAL;
+            World.S.transform.rotation = this.transform.rotation;
+        } else {
+            Vector3 newRotation = Vector3.Lerp(startAutoRotationValue, endAutoRotationValue, t);
+            transform.rotation = Quaternion.Euler(newRotation);
+            World.S.transform.rotation = this.transform.rotation;
+        }
+    }
+}
