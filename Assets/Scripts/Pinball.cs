2f23bd8cf4da3668ad422640b5b0201e80118689
Morgan Erlich <nagrom525@gmail.com>
2016-10-30 18:55:53 -0400

    screen bounds checking

---
+++ Assets/Scripts/Pinball.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pinball : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if(UtilityFunctions.ScreenBoundsCheck(this.GetComponent<SphereCollider>().bounds) != Vector3.zero) {
+            GameCanvasNavigation.S.ReloadLevel();
+        }
+	}
+}
